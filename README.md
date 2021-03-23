eaf62de2369d7a9207eed426dd06cbe0c2b1b20b
杜宇 <dygood@outlook.com>
2021-03-23 20:36:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Yarn2.x包管理工具
+
+必须安装Yarn 1.x才行,使用方法,解压到项目根目录即可使用.
+
+## Getting Started
+
+本项目由于项目中使用 yarn 2.x 进行包管理,所以建议大家使用[yarn 下载](https://classic.yarnpkg.com/latest.msi).npm 不能保证正常使用.[yarn 2.x 官网](https://yarnpkg.com/)
+
+更新依赖包
+
+```bash
+yarn upgrade-interactive
+```
+
+更新 yarn
+
+```bash
+yarn set version berry
+```
+
+导入 yarn 更新依赖包插件
+
+```bash
+yarn plugin import interactive-tools
+```
+
+导入 yarn TypeScript 库自动加入插件
+
+```bash
+yarn plugin import typescript
+```
---
+++ src/app.tsx
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import { Colors, DebugInstructions, Header, LearnMoreLinks, ReloadInstructions } from 'react-native/Libraries/NewAppScreen';
+import { SafeAreaView, ScrollView, StatusBar, Text, View, useColorScheme } from 'react-native';
+
+import React from 'react';
+import Section from 'components/Section';
+import styles from 'styles/app';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white
+          }}
+        >
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">Read the docs to discover what to do next:</Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
